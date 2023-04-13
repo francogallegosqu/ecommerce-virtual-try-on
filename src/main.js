2208d991e71b917e5e62c8b20bba24bed74f0ce0
Santana20 <sstfmscontreras@gmail.com>
2023-04-12 22:27:56 -0500

    primera version de clothCard

--- src/main.js
+++ src/main.js
@@ -6,8 +6,23 @@ import router from './router'
 
 import './assets/main.css'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
+
+/* add icons to the library */
+library.add(faHeart)
+
+
 const app = createApp(App)
 
+app.component('Fai', FontAwesomeIcon)
 app.use(createPinia())
 app.use(router)
 
--- src/views/ClothsView.vue
+++ src/views/ClothsView.vue
@@ -1,9 +1,27 @@
-<script setup>
+<script>
 import ClothCard from '../components/ClothCard.vue';
+
+export default {
+  data() {
+    return {
+      card_obj : {
+        url_img: 'https://cdn.pixabay.com/photo/2010/12/13/10/05/berries-2277_640.jpg',
+        name: 'polo',
+        price: 50,
+      },
+    }
+  },
+  components: {
+    ClothCard
+  }
+}
+
 </script>
 
 <template>
   <main>
-    <ClothCard />
+    <div class="w-full bg-slate-400 h-full py-5 flex justify-center items-center">
+      <ClothCard :card="card_obj" />
+    </div>
   </main>
 </template>
