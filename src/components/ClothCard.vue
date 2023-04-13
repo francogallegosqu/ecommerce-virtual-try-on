2e04e55b5f3f5cb88b263ed4664b45f83f4172bc
unknown <Proyecto@H40-09.upc.edu.pe>
2023-04-13 17:23:07 -0500

    segunda version de ClothCard y v-for en ClothView

--- src/components/ClothCard.vue
+++ src/components/ClothCard.vue
@@ -1,6 +1,6 @@
 <script setup>
 defineProps({
-  card: {
+  cloth: {
     type: Object,
     required: true
   }
@@ -10,23 +10,23 @@ defineProps({
 <template>
   <div
     class="relative pl-1 flex justify-center rounded-xl hover:scale-[1.01] duration-500 transform transition cursor-pointer">
-    <!-- Boton "Favoritos" -->
-    <a
-      class="absolute top-0 right-0 mt-3 mr-1 z-30 hover:text-gray-100 w-8 h-8 bg-white text-black rounded-full text-center">
-      <Fai icon="fa-regular fa-heart" class="fa-xl pt-1"/>
-    </a>
-    <div class="w-64 pb-2 bg-white rounded-xl shadow-xl z-10">
-
+    <div class="pb-2 bg-white rounded-xl shadow-xl z-10">
+      <!-- Boton "Favoritos" -->
+      <a
+        class="absolute top-0 right-0 mt-3 mr-1 z-30 hover:text-gray-100 w-8 h-8 bg-white text-black rounded-full text-center">
+        <Fai icon="fa-regular fa-heart" class="fa-xl pt-1"/>
+      </a>
+      
       <div class="relative">
-        <img :src="card.url_img" class="max-h-60 object-cover rounded-t-xl" alt="">
+        <img :src="cloth.url_img" class="max-h-60 object-cover rounded-t-xl" alt="">
       </div>
 
       <div class="px-2 py-1">
         <!-- Product Title -->
-        <div class="text-sm md:text-base font-bold pr-2">{{ card.name }}</div>
+        <div class="text-sm md:text-base font-bold pr-2">{{ cloth.name }}</div>
         <!-- Precio -->
-        <p class="pb-1 md:pb-2 text-xs md:text-sm text-gray-500">{{ `S/. ${card.price}` }}</p>
-        <!-- Tombol pesan -->
+        <p class="pb-1 md:pb-2 text-xs md:text-sm text-gray-500">{{ `S/. ${cloth.price}` }}</p>
+        <!-- Boton probarse -->
         <a class="inset-x-0 bottom-0 flex justify-center bg-[#FEE7D6] hover:bg-white text-sm md:text-base border hover:border-2 hover:border-[#FEE7D6] rounded-xl px-5 p-1 text-black"
           href="#">Probarse</a>
       </div>
--- src/views/ClothsView.vue
+++ src/views/ClothsView.vue
@@ -4,11 +4,73 @@ import ClothCard from '../components/ClothCard.vue';
 export default {
   data() {
     return {
-      card_obj : {
-        url_img: 'https://cdn.pixabay.com/photo/2010/12/13/10/05/berries-2277_640.jpg',
-        name: 'polo',
-        price: 50,
-      },
+      clothes: [
+        {
+          id: 1,
+          url_img: 'https://cdn.pixabay.com/photo/2010/12/13/10/05/berries-2277_640.jpg',
+          name: 'polo',
+          price: 50,
+        },
+        {
+          id: 2,
+          url_img: 'https://cdn.pixabay.com/photo/2010/12/13/10/05/berries-2277_640.jpg',
+          name: 'pantalon',
+          price: 65,
+        },
+        {
+          id: 3,
+          url_img: 'https://cdn.pixabay.com/photo/2010/12/13/10/05/berries-2277_640.jpg',
+          name: 'pantalon',
+          price: 65,
+        },
+        {
+          id: 4,
+          url_img: 'https://cdn.pixabay.com/photo/2010/12/13/10/05/berries-2277_640.jpg',
+          name: 'pantalon',
+          price: 65,
+        },
+        {
+          id: 5,
+          url_img: 'https://cdn.pixabay.com/photo/2010/12/13/10/05/berries-2277_640.jpg',
+          name: 'pantalon',
+          price: 65,
+        },
+        {
+          id: 6,
+          url_img: 'https://cdn.pixabay.com/photo/2010/12/13/10/05/berries-2277_640.jpg',
+          name: 'pantalon',
+          price: 65,
+        },
+        {
+          id: 7,
+          url_img: 'https://cdn.pixabay.com/photo/2010/12/13/10/05/berries-2277_640.jpg',
+          name: 'pantalon',
+          price: 65,
+        },
+        {
+          id: 8,
+          url_img: 'https://cdn.pixabay.com/photo/2010/12/13/10/05/berries-2277_640.jpg',
+          name: 'pantalon',
+          price: 65,
+        },
+        {
+          id: 9,
+          url_img: 'https://cdn.pixabay.com/photo/2010/12/13/10/05/berries-2277_640.jpg',
+          name: 'pantalon',
+          price: 65,
+        },
+        {
+          id: 10,
+          url_img: 'https://cdn.pixabay.com/photo/2010/12/13/10/05/berries-2277_640.jpg',
+          name: 'pantalon',
+          price: 65,
+        },
+      ],
+      // card_obj : {
+      //   url_img: 'https://cdn.pixabay.com/photo/2010/12/13/10/05/berries-2277_640.jpg',
+      //   name: 'polo',
+      //   price: 50,
+      // },
     }
   },
   components: {
@@ -20,8 +82,10 @@ export default {
 
 <template>
   <main>
-    <div class="w-full bg-slate-400 h-full py-5 flex justify-center items-center">
-      <ClothCard :card="card_obj" />
+    <div class="mx-5 h-full py-5 justify-center items-center grid grid-cols-5 gap-5">
+      <div v-for="cloth in clothes" :key="cloth.id">
+        <ClothCard  :cloth="cloth" />
+      </div>
     </div>
   </main>
 </template>
